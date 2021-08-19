e28137ed75767e66cc4303fd9e3c9348f9f1bb19
Elisa Shapiro <elisashapiro@gmail.com>
2021-08-19 10:11:26 -0400

    some methods done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,12 @@
 class Company < ActiveRecord::Base
     has_many :freebie
     has_many :dev, through: :freebie
+
+    def give_freebie(dev, item_name, value)
+        new_freebie = Freebie.create(dev_id: dev.id, item_name: item_name, value: value)
+    end
+
+    def self.oldest_company
+        self.all.min{|a, b| a.founding_year <=> b.founding_year}
+    end
 end
