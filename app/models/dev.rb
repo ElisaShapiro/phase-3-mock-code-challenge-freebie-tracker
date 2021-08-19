b63ce7ecb9b9a316f7f39a2a090636d4964dda9e
Elisa Shapiro <elisashapiro@gmail.com>
2021-08-19 10:28:08 -0400

    completed delivs commit

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,18 @@
 class Dev < ActiveRecord::Base
     has_many :freebie
     has_many :company, through: :freebie
+
+    def received_one?(item_name)
+        self.freebie.filter{|own_freebie|
+            if item_name == own_freebie.item_name
+                return true
+            else
+                return false
+            end
+        }
+    end
+
+    def give_away(dev, freebie)
+        Freebie.update(freebie.id, :dev_id => dev.id)
+    end
 end
